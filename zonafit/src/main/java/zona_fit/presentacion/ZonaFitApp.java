3cbcd43eca12f2bdc6d6dea55dc8f7dba5097bc2
Josecode10 <chema1987@hotmail.com>
2025-08-23 17:17:46 -0500

    Curso Java

---
+++ zonafit/src/main/java/zona_fit/presentacion/ZonaFitApp.java
@@ -0,0 +1,10 @@
+package zona_fit.presentacion;
+
+public class ZonaFitApp {
+    public static void main(String[] args) {
+        // Crear un menú que contenga las siguientes opciones
+        // Listar, agregar , modificar (solicitar el id), eliminar (soliciar id)
+        
+    }
+
+}
