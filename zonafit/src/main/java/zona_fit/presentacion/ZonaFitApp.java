32fd9f24bd9965fa90635d5bb55e7e9c57913be6
Josecode10 <chema1987@hotmail.com>
2025-08-24 10:14:39 -0500

    App zonafit

--- zonafit/src/main/java/zona_fit/presentacion/ZonaFitApp.java
+++ zonafit/src/main/java/zona_fit/presentacion/ZonaFitApp.java
@@ -1,10 +1,58 @@
 package zona_fit.presentacion;
 
+import java.util.Scanner;
+
 public class ZonaFitApp {
     public static void main(String[] args) {
-        // Crear un menú que contenga las siguientes opciones
-        // Listar, agregar , modificar (solicitar el id), eliminar (soliciar id)
-        
+        // Crear un objeto Scanner para leer la entrada del usuario
+        var teclado = new Scanner(System.in);
+        System.out.println("\nBienvenido a ZonaFit\n");
+        var continuar = false; // Variable para controlar el bucle del menú
+        while (!continuar) { // Bucle del menú
+            var opcion = mostrarMenu(teclado); // Mostrar el menú y obtener la opción del usuario
+            switch (opcion) {
+                case 1 -> listarClientes(teclado); 
+                case 2 -> agregarcliente(teclado);
+                case 3 -> modificarCliente(teclado);
+                case 4 -> eliminarCliente(teclado);
+                case 5 -> {
+                    System.out.println("Has salido del sistema. ¡Hasta luego!");
+                    continuar = true; // Salir del bucle
+                }
+                default -> System.out.println("Opción no válida");
+            }
+        }
+
+
     }
+
+    // Mostrar el menú y devolver la opción seleccionada
+    static int mostrarMenu(Scanner teclado){
+        var opcion = 0;
+        System.out.println("""
+                Qué deseas hacer:
+                1) Listar clientes
+                2) Agregar cliente
+                3) Modificar cliente
+                4) Eliminar cliente
+                5) Salir
+                Escoge una opcion:\s
+                """);
+        try {
+            opcion = Integer.parseInt(teclado.nextLine());
+            return opcion;
+        } catch (Exception e) {
+            System.out.println("Se debe ingresar un entero: " + e.getMessage());
+        }
+        return opcion;
+    }
+
+    static void listarClientes(Scanner teclado) {}
+
+    static void agregarcliente(Scanner teclado) {}
+
+    static void modificarCliente(Scanner teclado) {}
+
+    static void eliminarCliente(Scanner teclado) {}
 
 }
