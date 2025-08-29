2817a7c3c0b63224d7f2e39e7e4902ac9e98fc7e
Josecode10 <chema1987@hotmail.com>
2025-08-28 20:13:56 -0500

    Refactor logging configuration in logback-spring.xml for improved clarity

--- ZonaFitSpring/src/main/java/gm/zona_fit/ZonaFitApplication.java
+++ ZonaFitSpring/src/main/java/gm/zona_fit/ZonaFitApplication.java
@@ -1,5 +1,7 @@
 package gm.zona_fit;
 
+import java.util.Scanner;
+
 // import java.util.logging.Logger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import gm.zona_fit.servicio.IClienteServicio;
+import lombok.var;
 
 @SpringBootApplication // indica que es una aplicación de Spring Boot
 public class ZonaFitApplication implements CommandLineRunner{
@@ -17,18 +20,40 @@ public class ZonaFitApplication implements CommandLineRunner{
 
 	private static final Logger logger = LoggerFactory.getLogger(ZonaFitApplication.class);
 
+	String nl = System.lineSeparator();
 
 	public static void main(String[] args) {
 		logger.info("Iniciando la aplicacion");
 		// Levantar la fábrica de Spring
 		SpringApplication.run(ZonaFitApplication.class, args);
-		logger.info("¡Aplicacion finalizada!");
+		logger.info("Aplicacion finalizada!");
 	}
-
 
 	@Override
 	public void run(String... args) throws Exception {
-		logger.info("*** Aplicacion Zona Fit (GYM) ***");
+		zonaFitApp();
+	}
+	
+	private void zonaFitApp(){
+		var salir = false;
+		var consola = new Scanner(System.in);
+		while (!salir) {
+			var opcion = mostrarMenu(consola);
+			// salir = ejecutarOpciones(consola, opcion);
+			logger.info(nl);
+		}
 	}
 
+	private int mostrarMenu(Scanner consola){
+		logger.info(nl + """
+			*** Aplicacion Zona Fit (GYM) ***
+			1. Listar clientes
+			2. Buscar cliente
+			3. Agregar cliente
+			4. Modificar cliente
+			5. Eliminar cliente
+			6. Salir
+			Elige una opcion:\s""");
+		return Integer.parseInt(consola.nextLine());
+	}
 }
